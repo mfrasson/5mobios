9a42e5d3b6acff1faf100a2d295bd0971ccd8b03
Usuário Convidado <Guest@L602MICRO20.local>
2014-10-27 20:37:05 -0200

    commit

---
+++ mtg_waff/ListaResultadoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LIstaResultadoViewController.swift
+//  mtg_waff
+//
+//  Created by Usuário Convidado on 27/10/14.
+//  Copyright (c) 2014 WAFF Magic. All rights reserved.
+//
+
+import UIKit
+
+class ListaResultadoViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view. denovo
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func temp(){
+        
+    }
+    
+}
