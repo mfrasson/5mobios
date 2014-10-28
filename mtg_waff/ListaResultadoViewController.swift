f3b77684a9aa42363d86031d6518021f421d430f
Usuário Convidado <Guest@L602MICRO17.local>
2014-10-27 22:39:27 -0200

    add ListViewController

--- mtg_waff/ListaResultadoViewController.swift
+++ mtg_waff/ListaResultadoViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class ListaResultadoViewController: UITableViewController {
+class ListaResultadoViewController: UITableViewController, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+    var cartas:Array<Dictionary<String, AnyObject>>? = nil
+    var data = NSMutableData()
+    
+    var nomePesquisa:NSString = "Lotus"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // TODO: Implementar a busca pelo nome informado na tela anterior...
+        let url:NSURL = NSURL(string: "http://api.mtgapi.com/v1/card/name/" + nomePesquisa)
+        
+        let request:NSURLRequest = NSURLRequest(URL: url)
+        
+        // Invoca a requisicao de forma sincrona
+        var error = NSErrorPointer()
+        var response = AutoreleasingUnsafeMutablePointer<NSURLResponse?>()
+        var data:NSData? = NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: error)
+        
+        //		//MARK: - JSON Steps
+        var json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: nil) as Array<AnyObject>
+        
+        self.cartas = json as? Array<Dictionary<String, AnyObject>>
+        
+        // TODO: Implementar a exibição do Resultado na TableView
+        println(self.cartas)
         
         // Do any additional setup after loading the view. denovo
     }
@@ -25,5 +47,4 @@ class ListaResultadoViewController: UITableViewController {
     func temp(){
         
     }
-    
 }
