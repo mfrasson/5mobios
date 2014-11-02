e468b8b729305fe7e0af5f929708a3b397bd9dfc
Ronaldo Faraone <ronfaraone@Mac-Pro-de-Ronaldo.local>
2014-11-02 21:09:53 -0200

    info view controller

---
+++ mtg_waff/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  mtg_waff
+//
+//  Created by Ronaldo Faraone on 02/11/14.
+//  Copyright (c) 2014 WAFF Magic. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
